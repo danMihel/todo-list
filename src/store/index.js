c58f5a1a3d2f11fb0b4c69bbb71fcd886d1815af
danMihel <d.mihel78@gmail.com>
2022-08-05 13:18:16 +0300

    main

--- src/store/index.js
+++ src/store/index.js
@@ -2,38 +2,7 @@ import { createStore } from "vuex";
 
 export default createStore({
   state: {
-    posts: [
-      {
-        id: "1",
-        body: "Размещение новостей на сайте",
-        date: "06.06.2020",
-        done: "Выполнено",
-      },
-      {
-        id: "2",
-        body: "Внедрить Wi-fi для читателей",
-        date: "25.03.2020",
-        done: "В работе",
-      },
-      {
-        id: "3",
-        body: "Отредактировать раздел “Доступная сред”",
-        date: "09.12.2020",
-        done: "В работе",
-      },
-      {
-        id: "4",
-        body: "Презентация “Информационные технологии”",
-        date: "02.03.2020",
-        done: "В работе",
-      },
-      {
-        id: "5",
-        body: "Форма сброса пароля",
-        date: "07.10.2020",
-        done: "В работе",
-      },
-    ],
+    posts: [],
     searchQuery: "",
     selectedSort: "",
     sortOptons: [
